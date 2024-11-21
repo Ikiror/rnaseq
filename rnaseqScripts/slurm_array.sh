c87495c1c1a21206f08a6232f227f4b5a74f30e3
Amo Ikiror <amoikiror@gmail.com>
2024-11-21 10:34:38 +0100

    modify script to module load instead of apptainer

--- rnaseqScripts/slurm_array.sh
+++ rnaseqScripts/slurm_array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --array=1-12
-#SBATCH --time=00:10:00
-#SBATCH --mem=1g
+#SBATCH --time=10:00:00
+#SBATCH --mem=16g
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=slurm_array
 #SBATCH --output=array_%J.out
@@ -14,21 +14,30 @@
 #added sbatch scripts to email me of beginning of execution, failure, and the end of the script.
 
 # define variables
-WORKDIR="/data/users/aikiror/rnaseq"
-OUTDIR="$WORKDIR/results"
-SAMPLELIST="$WORKDIR/metadata/samplelist.tsv"
+# FASTQCCONTAINER="/containers/apptainer/fastqc-0.12.1.sif"
+
+WORKINGDIR="/data/users/aikiror/rnaseq"
+OUTPUTFILESDIR="$WORKINGDIR/outputFiles/fastqcFiles"
+SAMPLELIST="$WORKINGDIR/rnaseqScripts/relativePathSampleList.tsv"
 
 SAMPLE=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $1; exit}' $SAMPLELIST`
 READ1=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $2; exit}' $SAMPLELIST`
 READ2=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $3; exit}' $SAMPLELIST`
 
-OUTFILE="$OUTDIR/${SAMPLE}.txt"
+OUTFILE="$OUTPUTFILESDIR/${SAMPLE}"
 
 ############################
 
+mkdir -p $OUTPUTFILESDIR
+mkdir -p $OUTFILE
 
-mkdir -p $OUTDIR
+#apptainer not working *should work now
+# apptainer exec --bind /data "$FASTQCCONTAINER" fastqc --outdir "$OUTFILE" "$READ1" "$READ2"
+#--bind makes the folder /data available to the fastqc container
 
-echo "Run task for $SAMPLE with $READ1 and $READ2 ..." > $OUTFILE
+# #echo "Run task for $SAMPLE with $READ1 and $READ2 ..." > $OUTFILE
 
+module load FastQC/0.11.9-Java-11  
+module load MultiQC/1.11-foss-2021a 
+fastqc $READ1 $READ2 -o $OUTFILE 
 sleep 30
